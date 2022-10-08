24b583e03be373c09671fb9b5548c85b8f25e168
BavlyMourad <bavleymourad@gmail.com>
2022-10-08 22:11:32 +0200

    added flutter slidable

--- lib/widgets/todo_list.dart
+++ lib/widgets/todo_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoList extends StatelessWidget {
   TodoList({super.key});
@@ -16,19 +17,61 @@ class TodoList extends StatelessWidget {
       itemCount: todos.length,
       itemBuilder: (context, index) {
         return Card(
-          margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 14.0),
-          surfaceTintColor: Colors.white,
+          margin: const EdgeInsets.symmetric(
+            vertical: 10.0,
+            horizontal: 14.0,
+          ),
+          surfaceTintColor: Colors.white30,
+          color: Colors.white30,
           elevation: 2.0,
-          child: ListTile(
-            leading: Checkbox(
-              value: false,
-              onChanged: (value) {},
+          child: Slidable(
+            startActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              children: [
+                SlidableAction(
+                  // An action can be bigger than the others.
+                  flex: 2,
+                  onPressed: (context) {},
+                  backgroundColor: const Color(0xFF7BC043),
+                  foregroundColor: Colors.white,
+                  icon: Icons.edit,
+                  label: 'Edit',
+                ),
+              ],
             ),
-            title: Text(
-              todos[index],
-              style: const TextStyle(
-                color: Color(0xFF303030),
-              ),
+            endActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              children: [
+                SlidableAction(
+                  // An action can be bigger than the others.
+                  flex: 2,
+                  onPressed: (context) {},
+                  backgroundColor: const Color(0xFFFE4A49),
+                  foregroundColor: Colors.white,
+                  icon: Icons.delete,
+                  label: 'Delete',
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  leading: Checkbox(
+                    value: false,
+                    onChanged: (value) {},
+                  ),
+                  title: Text(
+                    todos[index],
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Text(
+                  '2022-10-08',
+                ),
+              ],
             ),
           ),
         );
